5e4cf8dc4ca22c6dd763c9ff3278eecf0338fc0e
obslot777 <52664896+obslot777@users.noreply.github.com>
2025-06-21 09:04:25 +0000

    test

---
+++ script.js
@@ -0,0 +1,35 @@
+let nextNumber = 1;
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function setupGame() {
+  const game = document.getElementById("game");
+  game.innerHTML = "";
+  const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  shuffle(numbers);
+
+  numbers.forEach(num => {
+    const button = document.createElement("button");
+    button.textContent = num;
+    button.addEventListener("click", () => checkNumber(button, num));
+    game.appendChild(button);
+  });
+}
+
+function checkNumber(button, num) {
+  if (num === nextNumber) {
+    button.classList.add("correct");
+    button.disabled = true;
+    if (nextNumber === 9) {
+      document.getElementById("message").textContent = "クリア！おめでとう！";
+    }
+    nextNumber++;
+  }
+}
+
+setupGame();
